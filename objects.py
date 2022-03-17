33ca130b79e0f086d31226fcb4706d4105b7dc76
William Ryu Gyotoku <wrgyotoku@gmail.com>
2022-03-17 11:34:05 -0300

    adicionou-se class margem para o background

--- objects.py
+++ objects.py
@@ -42,3 +42,37 @@ class Bird:
         pass
     def update(self):
         pass
+
+'''import pygame
+import os
+import random
+
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))'''
+
+#um lado da margem....colocar outra flipada
+class Margem:
+    VELOCIDADE = 5
+    ALTURA = IMAGEM_MARGEM.get_width()
+    IMAGEM = IMAGEM_MARGEM
+
+    #passa a coordenada x e y1 e y1 vem na sequencia 
+    def __init__(x, self):
+        self.x = x
+        self.y1 = 0
+        self.y2 = self.ALTURA
+    # y1 1 imagem descendo e na sequencia desce a 2
+    def mover(self):
+        self.y1 -= self.VELOCIDADE
+        self.y2 -= self.VELOCIDADE
+
+        if self.y1 + self.ALTURA > 0:
+            self.y1 = self.y2 + self.ALTURA
+        if self.y2 + self.ALTURA > 0:
+            self.y2 = self.y1 + self.ALTURA
+
+    def desenhar(self, tela):
+        tela.blit(self.IMAGEM, (self.x, self.y1))
+        tela.blit(self.IMAGEM, (self.x, self.y2))
